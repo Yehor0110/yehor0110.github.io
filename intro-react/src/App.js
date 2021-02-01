8d61e603bf6ae45030dd49069eb73c8c18a55b09
Yehor Valentinovych <mcshka111@gmail.com>
2021-02-01 22:40:35 +0200

    INTRO-REACT - fixed className

--- intro-react/src/App.js
+++ intro-react/src/App.js
@@ -4,13 +4,13 @@ import './App.css';
 
 const HeaderComponent = () => {
   return (
-    <header className="main">
+    <header className="Main">
 			<a href="index.html" id="logo">
 				<img src="img/logo.svg" alt="logotype"/>
 			</a>
-      <nav className="navigation">
-        <ul className="menu">
-          <li><a href="#progect-name" className="active">Progect name</a></li>
+      <nav className="Navigation">
+        <ul className="Menu">
+          <li><a href="#progect-name" className="Active">Progect name</a></li>
           <li><a href="#description">Description</a></li>
           <li><a href="#workspace">Workspace</a></li>
           <li><a href="#main-footer">Footer</a></li>
@@ -23,14 +23,14 @@ const HeaderComponent = () => {
 class AboutSection extends React.Component {
   render () {
     return (
-      <section className="about" id="progect-name">
-        <div className="about-bgd">
-          <div className="about-overlay">
+      <section className="About" id="progect-name">
+        <div className="About-bgd">
+          <div className="About-overlay">
             <h1>React Internship</h1>
             <p>
                 JavaScript, HTML/CSS , на закінчення смакуватимемо React!
             </p>
-            <a href="#main-footer" className="button-about">Лечу до вас!</a>
+            <a href="#main-footer" className="Button-about">Лечу до вас!</a>
           </div>
         </div>	
       </section>
@@ -42,12 +42,12 @@ class DescriptionSection extends React.Component {
   render () {
     return (
       <section id="description">
-        <div className="description-bgd">
-          <div className="description-overlay">
-            <div className="image">
+        <div className="Description-bgd">
+          <div className="Description-overlay">
+            <div className="Image">
                 <img src="img/ourStoryLogo.png" alt="logotype"/>
             </div>
-            <div className="content">
+            <div className="Content">
               <h2>Lorem ipsum dolor sit</h2>
               <p>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. 
@@ -76,17 +76,17 @@ class WorkSpaceSection extends React.Component {
       <section id="workspace">
         <h2>Lorem ipsum dolor sit amet</h2>
         <h4>Aperiam totam neque esse enim</h4>
-        <div className="services-block">
-          <div className="service">
-              <a href="servises.html#box8-1">
-                  <img src="img/icons/1.png" alt="web design"/>
-                  <h3>Lorem ipsum dolor sit amet consectetur</h3>
-                  <p>
-                      Fugiat doloremque, commodi quis voluptates harum
-                  </p>
-              </a>		
+        <div className="Services-block">
+          <div className="Service">
+            <a href="servises.html#box8-1">
+              <img src="img/icons/1.png" alt="web design"/>
+              <h3>Lorem ipsum dolor sit amet consectetur</h3>
+              <p>
+                Fugiat doloremque, commodi quis voluptates harum
+              </p>
+            </a>		
           </div>
-          <div className="service">
+          <div className="Service">
               <a href="servises.html#box1-1">
                   <img src="img/icons/2.png" alt="BRANDING IDENTITY"/>
                   <h3>Lorem ipsum dolor sit amet consectetur</h3>
@@ -95,7 +95,7 @@ class WorkSpaceSection extends React.Component {
                   </p>
               </a>		
           </div>
-          <div className="service">
+          <div className="Service">
               <a href="servises.html#box2-1">
                   <img src="img/icons/3.png" alt="MOBILE APP"/>
                   <h3>Lorem ipsum dolor sit amet consectetur</h3>
@@ -104,7 +104,7 @@ class WorkSpaceSection extends React.Component {
                   </p>
               </a>	
           </div>
-          <div className="service">
+          <div className="Service">
               <a href="servises.html#box10-1">
                   <img src="img/icons/4.png" alt="SEARCH ENGINE OPTIMIZATION"/>
                   <h3>Lorem ipsum dolor sit amet consectetur</h3>
@@ -113,7 +113,7 @@ class WorkSpaceSection extends React.Component {
                   </p>
               </a>	
           </div>
-          <div className="service">
+          <div className="Service">
               <a href="servises.html#box7-1">
                   <img src="img/icons/5.png" alt="GAME DEVELOPMENT"/>
                   <h3>Lorem ipsum dolor sit amet consectetur</h3>
@@ -122,7 +122,7 @@ class WorkSpaceSection extends React.Component {
                   </p>
               </a>	
           </div>
-          <div className="service">
+          <div className="Service">
               <a href="servises.html#box9-1">
                   <img src="img/icons/6.png" alt="MADE WITH LOVE"/>
                   <h3>Lorem ipsum dolor sit amet consectetur</h3>
@@ -131,7 +131,7 @@ class WorkSpaceSection extends React.Component {
                   </p>
               </a>	
           </div>
-          <div className="clearfix"></div>
+          <div className="Clearfix"></div>
         </div>
       </section>
     );
@@ -141,15 +141,15 @@ class WorkSpaceSection extends React.Component {
 const MainFooter = () => {
   return (
     <footer id="main-footer">
-			<div className="footer-bgd">
-				<div className="footer-overlay">
-          <div className="author">
+			<div className="Footer-bgd">
+				<div className="Footer-overlay">
+          <div className="Author">
               <h3>Yehor Valentiukevych</h3>
-              <div className="wrap-links">
-                  <a href="https://github.com/Yehor0110/yehor0110.github.io" className="git-link" target="_blank" title="GitHub">
+              <div className="Wrap-links">
+                  <a href="https://github.com/Yehor0110/yehor0110.github.io" className="Git-link" target="_blank" title="GitHub">
                       <img src="img/icons/github.png" alt="GitHub"/>
                   </a>
-                  <a href="http://psd-html-css.ru/sites/default/files/public/old/demo/tajem-rev.png" title="Origin Design" target="_blank" className="design-link">
+                  <a href="http://psd-html-css.ru/sites/default/files/public/old/demo/tajem-rev.png" title="Origin Design" target="_blank" className="Design-link">
                       <img src="img/icons/design.png" alt="Origin Design"/>
                   </a>
               </div>
