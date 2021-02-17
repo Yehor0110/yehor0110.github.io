a206a997ce36eb6bb821d446980edeaa5702777a
Yehor Valentinovych <mcshka111@gmail.com>
2021-02-17 21:58:45 +0200

    fixed - added conditionals and variables

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,20 +1,84 @@
 import React from 'react';
 
-class AboutSection extends React.Component {
-    render () {
-      const h1 = "React Internship";
-      const aboutText = "JavaScript, HTML/CSS , на закінчення смакуватимемо React!";
-      const aboutButton = "Лечу до вас!";
+function Welcome(props) {
+  const h1 = "React Internship";
+  return <h1>{h1}</h1>;
+}
 
+function Text() {
+  let aboutText = "JavaScript, HTML/CSS , на закінчення смакуватимемо React!";
+  var level = 2;
+
+  return (
+    <div className="conteiner">
+      <p>{aboutText}</p>
+      <p>Level - {level}</p>
+    </div>
+  );
+  
+}
+
+function Button() {
+  let showButton = 'show';
+  const errorMassage = "ошибочка вышла";
+
+  if(showButton === 'show') {
+    return <a href="#main-footer" className="Button-about show">{showButton}</a>
+  } else {
+    return <a href="#main-footer" className="Button-about error">{errorMassage}</a>
+  }
+
+}
+
+function WarningBanner(props) {
+  if (!props.warn) {
+    return null;
+  }
+
+  return (
+    <div className="warning">
+      Warning!
+    </div>
+  );
+}
+
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {showWarning: false}
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+  }
+
+  handleToggleClick() {
+    this.setState(prevState => ({
+      showWarning: !prevState.showWarning
+    }));
+  }
+  
+  render() {
+    return (
+      <div>
+        <WarningBanner warn={this.state.showWarning} />
+        <button onClick={this.handleToggleClick}>
+          {this.state.showWarning ? 'Hide' : 'Show'}
+        </button>
+      </div>
+    );
+  }
+}
+
+class AboutSection extends React.Component {
+  // 
+    render () {
       return (
         <section className="About" id="Progect-name">
+        
           <div className="About-bgd">
             <div className="About-overlay">
-              <h1>{h1}</h1>
-              <p>
-                {aboutText}
-              </p>
-              <a href="#main-footer" className="Button-about">{aboutButton}</a>
+            <Page></Page>
+              <Welcome></Welcome>
+              <Text></Text>
+              <Button></Button>
             </div>
           </div>	
         </section>
